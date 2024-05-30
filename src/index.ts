34556f449cafecec0e62af2f07003f0267a65d35
Emanuel <59392453+Emsa001@users.noreply.github.com>
2024-05-30 23:58:48 +0200

    comment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
+// Import necessary modules
 import "dotenv/config";
-import IntraRequest from "./intra";
-
-const request = new IntraRequest(null);
-const user = await request.get(`/v2/users/escura`, {});
-
-console.log(user);
